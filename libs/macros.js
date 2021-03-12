dde42c65ae7d96ab1a8ee6e4046f6bb002a9078c
Martin Hinz <martin.hinz@iaw.unibe.ch>
2021-03-12 19:13:37 +0100

    added book

--- libs/macros.js
+++
@@ -1,9 +0,0 @@
-remark.macros.width = function (width) {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + width + '" />';
-};
-
-remark.macros.height = function (height) {
-  var url = this;
-  return '<img src="' + url + '" style="height: ' + height + '" />';
-};
